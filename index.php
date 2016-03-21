7739f21828a418bdc45c6b14af53ee6f87ebd346
Marc Hernández Navarro <marcmhn@hotmail.com>
2016-03-21 11:38:34 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP File Model Example</title>
+</head>
+<body>
+	<form method="post" enctype="multipart/form-data" action="uploadfile.php">
+		<input type="file" name="testfile" value="Select a file" />
+		<input type="submit" value="Upload" />
+	</form>
+</body>
+</html>
