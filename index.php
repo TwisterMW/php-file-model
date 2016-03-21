da9d499277ccbf107b8e52599e0a0e9b7998a279
Marc Hernández Navarro <marcmhn@hotmail.com>
2016-03-21 16:05:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>PHP File Model Example</title>
 </head>
 <body>
-	<form method="post" enctype="multipart/form-data" action="uploadfile.php">
+	<form method="post" enctype="multipart/form-data" action="php/test-uploadfile.php">
 		<input type="file" name="testfile" value="Select a file" />
 		<input type="submit" value="Upload" />
 	</form>
